502b273a919d94d32f0597348440ebb26c5aedef
Ido Zilberberg <idoz@orbs.com>
2019-05-22 12:23:09 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+// const port = 3000
+
+//app.get('/', (req, res) => res.send('Hello, I am a Wallet!'))
+
+//app.listen(port, () => console.log(`Started listening on port ${port}!`))
+
+
+main();
+
+function main() {
+	// do everything here
+	console.log("main() started");
+}
