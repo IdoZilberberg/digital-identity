a0d141ac434a213225939d2072e30b5a3334f926
OdedW <odedw@orbs.com>
2019-05-23 14:40:39 +0300

    contract

--- proof-verifier/verifier/contract/proof_contract.go
+++ proof-verifier/verifier/contract/proof_contract.go
@@ -11,7 +11,7 @@ import (
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk/v1"
 )
 
-var PUBLIC = sdk.Export(CheckProof)
+var PUBLIC = sdk.Export(VerifyProof)
 var SYSTEM = sdk.Export(_init)
 
 func _init() {
@@ -57,12 +57,12 @@ func negate(curve curves.CurveSystem, g1point curves.Point) (result curves.Point
 	return
 }
 
-func verify(curve curves.CurveSystem, input []big.Int, proof proof) (result bool) {
+func verify(curve curves.CurveSystem, input []*big.Int, proof proof) (result bool) {
 	vk := verifyingKey(curve)
 	bigintCoordsZero := []*big.Int{big.NewInt(0), big.NewInt(0)}
 	vkX, _ := curve.MakeG1Point(bigintCoordsZero, true)
 	for i := 0; i < len(input); i++ {
-		tmp := vk.gammaABC[i+1].Mul(&input[i])
+		tmp := vk.gammaABC[i+1].Mul(input[i])
 		tmp2, _ := vkX.Add(tmp)
 		vkX = tmp2
 	}
@@ -84,20 +84,6 @@ func verify(curve curves.CurveSystem, input []big.Int, proof proof) (result bool
 	return
 }
 
-func VerifyProof(curve curves.CurveSystem, a [2]big.Int, b [2][2]big.Int, c [2]big.Int, input []big.Int) (result bool) {
-	var proof proof
-
-	coordsA := []*big.Int{&a[0], &a[1]}
-	proof.a, _ = curve.MakeG1Point(coordsA, true)
-	coordsB := []*big.Int{&b[0][0], &b[0][1], &b[1][0], &b[1][1]}
-	proof.b, _ = curve.MakeG2Point(coordsB, true)
-	coordsC := []*big.Int{&c[0], &c[1]}
-	proof.c, _ = curve.MakeG1Point(coordsC, true)
-
-	result = verify(curve, input, proof)
-	return
-}
-
 func parseBigIntArray(input string) (result []*big.Int) {
 	var inputStrings []string = strings.Split(input, ",")
 	var output []*big.Int = make([]*big.Int, len(inputStrings))
@@ -113,30 +99,55 @@ func Is_ok() bool {
 	return true
 }
 
-func CheckProof() uint32 {
-	var a [2]big.Int
-	a[0].SetString("0x09f50c87eebe68b181ea9772a125ac85efd91fb6e90209c1db8477873808d5d9",0)
-	a[1].SetString("0x1379940536b0a9739866ecfb81f58db136199752dd35efcaee26adbc76218f7c",0)
-	var b [2][2]big.Int
-	b[0][0].SetString("0x2f0c9a8098d33413550fef8561bb063300fc5ef5487de69d31d26854e789aaab",0)
-	b[0][1].SetString("0x0cc94227faedb1ec9624eac3f692fd1162e27d77a82e512a8e313cc9307294c7",0)
-	b[1][0].SetString("0x245720ccab66f9c32aceefd7b8be03b956aedf8a04c69453da8b36eec46a7a96",0)
-	b[1][1].SetString("0x2cabed2c5711b16117f94b9db65a1a42d669a5c895d551fca43f84a8a22f1271",0)
-	var c [2]big.Int
-	c[0].SetString("0x0daf9ac1b3d969873078d7b7de0f61f497b5bd6a4be0a25192b0127888f74b47",0)
-	c[1].SetString("0x1d1ef5e4db1992e9a1abf73c8cd7260c16617f22fbd143b0788622073a9c348e",0)
-	var input [7]big.Int
-	input[0].SetString("0x00000000000000000000000000000000000000000000000000000000000007e3",0)
-	input[1].SetString("0x0000000000000000000000000000000072d67d19ceb15c63291cce17f12d3f3e",0)
-	input[2].SetString("0x0000000000000000000000000000000062258940f07390a79bcdbc7e2f4c3124",0)
-	input[3].SetString("0x0000000000000000000000000000000000000000000000000000000000016062",0)
-	input[4].SetString("0x00000000000000000000000000000000b6964b2572dd8ca056313bd0d92cf572",0)
-	input[5].SetString("0x000000000000000000000000000000006ffd538fd64a3e68c1fe2a8d4b389232",0)
-	input[6].SetString("0x0000000000000000000000000000000000000000000000000000000000000001",0)
+//func CheckProof() uint32 {
+//	var a [2]big.Int
+//	a[0].SetString("0x09f50c87eebe68b181ea9772a125ac85efd91fb6e90209c1db8477873808d5d9", 0)
+//	a[1].SetString("0x1379940536b0a9739866ecfb81f58db136199752dd35efcaee26adbc76218f7c", 0)
+//	var b [2][2]big.Int
+//	b[0][0].SetString("0x2f0c9a8098d33413550fef8561bb063300fc5ef5487de69d31d26854e789aaab", 0)
+//	b[0][1].SetString("0x0cc94227faedb1ec9624eac3f692fd1162e27d77a82e512a8e313cc9307294c7", 0)
+//	b[1][0].SetString("0x245720ccab66f9c32aceefd7b8be03b956aedf8a04c69453da8b36eec46a7a96", 0)
+//	b[1][1].SetString("0x2cabed2c5711b16117f94b9db65a1a42d669a5c895d551fca43f84a8a22f1271", 0)
+//	var c [2]big.Int
+//	c[0].SetString("0x0daf9ac1b3d969873078d7b7de0f61f497b5bd6a4be0a25192b0127888f74b47", 0)
+//	c[1].SetString("0x1d1ef5e4db1992e9a1abf73c8cd7260c16617f22fbd143b0788622073a9c348e", 0)
+//	var input [7]big.Int
+//	input[0].SetString("0x00000000000000000000000000000000000000000000000000000000000007e3", 0)
+//	input[1].SetString("0x0000000000000000000000000000000072d67d19ceb15c63291cce17f12d3f3e", 0)
+//	input[2].SetString("0x0000000000000000000000000000000062258940f07390a79bcdbc7e2f4c3124", 0)
+//	input[3].SetString("0x0000000000000000000000000000000000000000000000000000000000016062", 0)
+//	input[4].SetString("0x00000000000000000000000000000000b6964b2572dd8ca056313bd0d92cf572", 0)
+//	input[5].SetString("0x000000000000000000000000000000006ffd538fd64a3e68c1fe2a8d4b389232", 0)
+//	input[6].SetString("0x0000000000000000000000000000000000000000000000000000000000000001", 0)
+//
+//	if VerifyProof(curves.Altbn128, a, b, c, input[:]) {
+//		return 1
+//	} else {
+//		return 0
+//	}
+//}
 
-	if (VerifyProof(curves.Altbn128, a, b, c, input[:])) {
-		return 1
+func VerifyProof(aStr string, bStr string, cStr string, inputStr string) uint32 {
+	var proof proof
+	//aStr= "0x075803fed276c64417f0a7ae76c1b2c5bc6d8efd7466c4e791ed8a5b442b2d9e,0x0e73a4c3932fc0a54c9f6e38c87067ecd40a27aec681a91e1f0a10ecb52f5516"
+	//bStr = "0x1dd811ab5d50b91946cd1db42bc2a393484ecfbc8cd5920065ea94fde6d5ba55,0x1ae9b76c87c3b9cafbb4e1d39b4804b860a0ca1dafacdf208424bbe326c8cd2c,0x154086af81180da566c76d80a5a4b71d50f6f81c0c74bbb5565ba0def8e1b1ce,0x28da38e7ab834c8a6fc2176a393868bf5fbfabb0152c0444ee28873fe1773571"
+	//cStr = "0x13b1cba32cd29f3568aa35212a22943085f0e40fa80f2719c3506e5a8a14310a,0x2d16fead425a374b3d6ded96491e8291a965d7f2afaf10f6ce3f0cfb3873c447"
+	//inputStr = "0x000000000000000000000000000000000000000000000000000000000001bba1,0x0000000000000000000000000000000000000000000000000000000000000001"
+
+	var a []*big.Int = parseBigIntArray(aStr)
+	proof.a, _ = curves.Altbn128.MakeG1Point(a, true)
+
+	var b []*big.Int = parseBigIntArray(bStr)
+	proof.b, _ = curves.Altbn128.MakeG2Point(b, true)
+
+	var c []*big.Int = parseBigIntArray(cStr)
+	proof.c, _ = curves.Altbn128.MakeG1Point(c, true)
+	var input []*big.Int = parseBigIntArray(inputStr)
+
+	result := verify(curves.Altbn128, input, proof)
+	if result {
+		return 0x10ded
 	} else {
-		return 0
+		return 0x1d0
 	}
 }
